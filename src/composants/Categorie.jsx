55d564a7fe5110f3a0130a0932f2e8f0b3321d24
yatte shop <yatteshop@gmail.com>
2025-07-12 19:47:54 +0000

    kat uno

--- src/composants/Categorie.jsx
+++ src/composants/Categorie.jsx
@@ -1,10 +1,21 @@
-
+import { useCategoryStore } from "../stores/Store"
+import {useRouter} from "next/router"
 
 export default function Categorie({ categories=[] }) {
+  
+  const router = useRouter();
+  
+  const {selectedCategory, setSelectedCategory} = useCategoryStore()
+  
+ const choixCategorie = (nom)=>{
+    setSelectedCategory(nom)
+    router.push("/produits")
+  } 
+  
   return (
     <div className="Categorie">
       {categories.map((item) => (
-        <div key={item.id} className="CategorieItem">
+        <div key={item.id} className="CategorieItem" onClick={()=>choixCategorie(item)}>
           <img src={item.image} alt={item.name} />
           <div className="CategorieNom">{item.name}</div>
         </div>
