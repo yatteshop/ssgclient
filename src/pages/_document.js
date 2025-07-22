90c421b12d214753bafa5daefc25a2e14bab5f99
yatte shop <yatteshop@gmail.com>
2025-07-22 22:24:58 +0000

    analy tree

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,5 @@
+/*
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -12,6 +14,35 @@ export default function Document() {
 
   gtag('config', 'G-V6M806HRBG');
 </script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+*/
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V6M806HRBG"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V6M806HRBG');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
       </Head>
       <body>
         <Main />
