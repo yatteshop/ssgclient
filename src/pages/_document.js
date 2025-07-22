e67a6b38e81adbe759393e5fb3f184adf4beb6e3
yatte shop <yatteshop@gmail.com>
2025-07-22 22:07:03 +0000

    analy one

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6M806HRBG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6M806HRBG');
+</script>
+      </Head>
       <body>
         <Main />
         <NextScript />
