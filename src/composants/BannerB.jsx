41d2f1bbdfa689885c00e196192836f7544349de
yatte shop <yatteshop@gmail.com>
2025-07-28 00:30:56 +0000

    goo x

--- src/composants/BannerB.jsx
+++ src/composants/BannerB.jsx
@@ -1,8 +1,18 @@
-
+import Head from 'next/head';
 
 export default function BannerB(){
   return(
     <div className="BannerB">
+      <Head>
+       <title>YATTE CÔTE D'IVOIRE</title>
+        <meta
+           name="description"
+           content="YATTE est une boutique en ligne innovante spécialisée dans la vente de produits de qualité au meilleur prix."
+        />
+       <meta name="robots" content="index, follow" />
+       <meta name="google-site-verification" content="HNNW2yZuf8plawr_kpuLSdi1PBeJAEkzayLcGCe4xgk" />
+        <link rel="canonical" href="https://yatteshop.netlify.app/" />
+      </Head>
       <div className="promoA">
         <div className="promoA1s">
           Qualités et prix imbattables sur nos électromenagers
