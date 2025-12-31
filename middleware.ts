96f45e674a910a6612496219f4f6ed5d9b3c065e
yatte shop <yatteshop@gmail.com>
2025-12-31 09:33:36 +0000

    norah 9

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 // middleware.js
 import { NextResponse } from "next/server";
-import  { NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
